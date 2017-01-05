2de55d7251d08d5a2be853e0653cc19de2c15e3c
Alex Bazlinton <abazlinton@gmail.com>
2017-01-05 15:04:21 +0000

    add couple of comments to code, remove .gitignore

--- index_markdown.rb
+++ index_markdown.rb
@@ -5,12 +5,14 @@ md_files = Hash.new{|hash, key| hash[key] = {} }
 
 Find.find(current_directory) do |path|
   if FileTest.directory?(path)
+    # ignore any directories starting with .
     if File.basename(path)[0] == "."
       Find.prune
     else
       next
     end
   else
+    # ignore any node_module directories
     if path[( path.size - 2 )..( path.size - 1 )] == "md" && path.split("/node_modules/").size == 1
         week_number_start = path.split("week_")[1]
         week_number = week_number_start.split("/")[0]

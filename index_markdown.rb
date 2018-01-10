7013b0dc57cf0a4f1481b296bb0a4450404ed941
Alex Bazlinton <abazlinton@gmail.com>
2018-01-10 17:32:34 +0000

    Update index_markdown.rb

--- index_markdown.rb
+++ index_markdown.rb
@@ -1,46 +1,18 @@
 require 'find'
 
 current_directory = Dir.pwd
-md_files = Hash.new{|hash, key| hash[key] = {} }
-
-Find.find(current_directory) do |path|
-  if FileTest.directory?(path)
-    # ignore any directories starting with .
-    if File.basename(path)[0] == "."
-      Find.prune
-    else
-      next
-    end
-  else
-    # ignore any node_module directories
-    if path[( path.size - 2 )..( path.size - 1 )] == "md" && path.split("/node_modules/").size == 1
-        week_number_start = path.split("week_")[1]
-        week_number = week_number_start.split("/")[0]
-        day_string = week_number_start.split("/")[1]
-        if md_files[week_number][day_string] == nil
-          md_files[week_number][day_string] = []
-        end
-        md_files[week_number][day_string] << "<a href=\"#{path}\">#{File.basename(path)}</a>"
-    else
-      next
-    end
-  end
-end
-
 output = open("classnotes_md_index.html", 'w')
 
-for week in 1..16
-  key = week.to_s
-  hash_for_week = md_files[key]
-  output.write("<h1>Week #{key}</h1>")
-
-  hash_for_week.each do |day_key, md_array|
-    output.write("<h2>#{day_key}</h2>")
-    md_array.each do |html|
-      output.write("<p>#{html}</p>")
+Find.find(current_directory) do |path|
+    parts = path.split("/")
+    if parts.last[-2..-1] == "md"
+      link_text = path.split(current_directory + "/").last
+      output.write("<p><a href=\"#{path}\">#{link_text}</a></p>")
     end
-  end
-
 end
 
 output.close
+
+
+
+
